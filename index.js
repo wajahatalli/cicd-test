810f109d7861325b7e926f9e24bc4ce27096c753
wajahat <wajahat.jutt09@gmail.com>
2025-02-27 12:44:56 +0000

    add

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Are you sure, you are running!\n");
+  res.end("Are you sure, you are running!\n");
+  res.end("Are you sure, you are running!\n");
 });
 
 server.listen(3000, () => {
