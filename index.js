4b1921b59d801e2cb40a26f702fed12201aa367b
wajahat <wajahat.jutt09@gmail.com>
2025-02-27 11:35:32 +0000

    edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Jenkins CI/CD!\n");
+  res.end("Are you sure, you are running!\n");
 });
 
 server.listen(3000, () => {
