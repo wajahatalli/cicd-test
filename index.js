75ff58b48e34183076079df9900c5b9dc0af6ce0
wajahat <wajahat.jutt09@gmail.com>
2025-02-27 12:59:20 +0000

    new add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Are you sure, you are running hunndjs!\n");
+  res.end("Are you sure, you are running h!\n");
 
 });
 
